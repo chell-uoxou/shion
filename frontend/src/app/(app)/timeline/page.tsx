b9377ba0679f39f5f52b142fa394027b32255b7b
eita <siitigo2006@gmail.com>
2025-08-22 20:22:18 +0900

    timelineの最初のおおまかなページを作り、全体のレイアウトの色を変えた

--- frontend/src/app/(app)/timeline/page.tsx
+++ frontend/src/app/(app)/timeline/page.tsx
@@ -1,9 +1,34 @@
 "use client";
-
+import { CirclePlus, Search } from "lucide-react";
 export default function Page() {
+  const hasMemory = false;
+
   return (
     <>
-      <div>timeline</div>
+      <div className="flex justify-center relative max-w-[90%] mx-auto">
+        <input
+          type="text"
+          className="w-full h-8 bg-white border-2 rounded-xl border-[#A19EA7] p-2"
+        />
+        <button
+          className="absolute right-0 m-1"
+          onClick={() => {
+            console.log("押した");
+          }}
+        >
+          <Search />
+        </button>
+      </div>
+      <div className="w-full h-full">
+        {hasMemory ? (
+          <div>メモリー一覧</div>
+        ) : (
+          <div className="h-full flex items-center justify-center ">
+            まだ出来事がありません
+          </div>
+        )}
+      </div>
+      <CirclePlus className="fill-[#7C56B5] text-[#F4EFFA] w-16 h-16 fixed bottom-18 right-5" />
     </>
   );
 }
