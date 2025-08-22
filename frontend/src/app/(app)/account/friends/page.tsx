de8ca251bca3ce9b76be857d30df87429feb6e98
yui-119 <poppop0206@icloud.com>
2025-08-23 03:07:33 +0900

    検索以外の構成完了

--- frontend/src/app/(app)/account/friends/page.tsx
+++ frontend/src/app/(app)/account/friends/page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import UserRow from "@/features/FriendListitem/page"; // パスは実際の場所に合わせて調整
+import { ChevronLeft } from "lucide-react";
 
 export default function Page() {
   return (
-    <main>
-      <h1>話し相手の一覧</h1>
-      <UserRow src="/user-icon.svg" name="ユーザー1" />
+    <main className="p-4 space-y-6">
+      <div className="relative flex items-center justify-center py-2">
+        <div className="absolute left-1">
+          <ChevronLeft size={28} className="cursor-pointer text-primary" />
+        </div>
+        <h1 className="text-2xl text-center text-primary">
+          話し相手一覧
+        </h1>
+      </div>
+      <div className="space-y-4">
+        <UserRow src="/user-icon.svg" name="ユーザー1" />
+      </div>
     </main>
   );
 }
+
