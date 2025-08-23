2ca8d4b2bd79551deba9e5667eb5e12ec05260b5
chell-uoxou <chelluoxou@gmail.com>
2025-08-23 18:56:32 +0900

    feat practiceを(app)内に移動

--- frontend/src/app/(app)/practice/page.tsx
+++ frontend/src/app/(app)/practice/page.tsx
@@ -3,10 +3,19 @@
 
 import { Counter } from "@/features/reactPractice/components/Counter";
 import { RedTitle } from "@/features/reactPractice/components/RedTitle";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function Page() {
+  const { user } = useAuth();
+
   return (
     <div className="p-5">
+      {user && (
+        <div>
+          <h2>Welcome, {user.name}!</h2>
+          <p>Email: {user.id}</p>
+        </div>
+      )}
       <div className="flex flex-col gap-4">
         <RedTitle text="soraのReact練習°˖✧◝(⁰▿⁰)◜✧˖°" />
         <RedTitle text="こんにちは" />
