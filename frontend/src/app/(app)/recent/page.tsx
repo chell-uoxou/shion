36118742dce52f0f295b8c786a104ceedc7c6604
sora-qwerty <sora.proof.of.guild@icloud.com>
2025-08-24 01:52:30 +0900

    recentやっと

--- frontend/src/app/(app)/recent/page.tsx
+++ frontend/src/app/(app)/recent/page.tsx
@@ -1,12 +1,35 @@
 "use client";
 
+import { SearchForm } from "@/features/memoryTimeline/components/SearchForm";
 import { RecentFriend } from "@/features/recent/RecentFriend";
 
 export default function Page() {
+  const friends = [
+    { id: 1, name: "ユーザー1", event: "出来事A", date: "7/28" },
+    { id: 2, name: "ユーザー2", event: "出来事B", date: "7/30" },
+    { id: 3, name: "ユーザー3", event: "出来事C", date: "8/1" },
+    { id: 4, name: "ユーザー4", event: "出来事D", date: "8/3" },
+    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
+    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
+    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
+    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
+    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
+  ];
   return (
-    <>
-      <div>recent</div>
-      <RecentFriend />
-    </>
+    <div className="h-full bg-[var(--brand-violet-1)]">
+      <SearchForm />
+      <div className="p-4">
+        <div className="space-y-3 max-h-160 overflow-y-auto">
+          {friends.map((friend) => (
+            <RecentFriend
+              key={friend.id}
+              name={friend.name}
+              event={friend.event}
+              date={friend.date}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
--- frontend/src/features/recent/RecentFriend.tsx
+++ frontend/src/features/recent/RecentFriend.tsx
@@ -2,17 +2,20 @@
 
 import React from "react";
 
-export function RecentFriend() {
+type RecentFriendProps = {
+  name: string;
+  event: string;
+  date: string;
+};
+export function RecentFriend(props: RecentFriendProps) {
   return (
     <div className="flex items-center space-x-4 pt-2 pb-2 m-2">
       <span className="w-15 h-15 items-center rounded-full bg-purple-600"></span>
       <div className="flex flex-col">
-        <span className="text-sm text-purple-600">ユーザー１</span>
-        <span className="text-xs text-gray-500 opacity-75">
-          出来事のタイトルを表示
-        </span>
+        <span className="text-sm text-purple-600">{props.name}</span>
+        <span className="text-xs text-gray-500 opacity-75">{props.event}</span>
       </div>
-      <span className="text-xs text-gray-500 ml-auto">8/6</span>
+      <span className="text-xs text-gray-500 ml-auto">{props.date}</span>
     </div>
   );
 }
