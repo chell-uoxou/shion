a8ecc72d0d9c66cad51c921d0e2776beb9e979e2
sora-qwerty <sora.proof.of.guild@icloud.com>
2025-08-24 04:13:26 +0900

    timelineできた

--- frontend/src/app/(app)/recent/page.tsx
+++ frontend/src/app/(app)/recent/page.tsx
@@ -5,15 +5,15 @@ import { RecentFriend } from "@/features/recent/RecentFriend";
 
 export default function Page() {
   const friends = [
-    { id: 1, name: "ユーザー1", event: "出来事A", date: "7/28" },
-    { id: 2, name: "ユーザー2", event: "出来事B", date: "7/30" },
-    { id: 3, name: "ユーザー3", event: "出来事C", date: "8/1" },
-    { id: 4, name: "ユーザー4", event: "出来事D", date: "8/3" },
-    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
-    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
-    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
-    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
-    { id: 4, name: "ユーザー5", event: "出来事E", date: "8/4" },
+    { id: "1", name: "ユーザー1", event: "出来事A", date: "7/28" },
+    { id: "2", name: "ユーザー2", event: "出来事B", date: "7/30" },
+    { id: "3", name: "ユーザー3", event: "出来事C", date: "8/1" },
+    { id: "4", name: "ユーザー4", event: "出来事D", date: "8/3" },
+    { id: "4", name: "ユーザー5", event: "出来事E", date: "8/4" },
+    { id: "4", name: "ユーザー5", event: "出来事E", date: "8/4" },
+    { id: "4", name: "ユーザー5", event: "出来事E", date: "8/4" },
+    { id: "4", name: "ユーザー5", event: "出来事E", date: "8/4" },
+    { id: "4", name: "ユーザー5", event: "出来事E", date: "8/4" },
   ];
   return (
     <div className="h-full bg-[var(--brand-violet-1)]">
@@ -23,6 +23,7 @@ export default function Page() {
           {friends.map((friend) => (
             <RecentFriend
               key={friend.id}
+              id={friend.id}
               name={friend.name}
               event={friend.event}
               date={friend.date}
--- frontend/src/app/(app)/timeline/page.tsx
+++ frontend/src/app/(app)/timeline/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { CirclePlus } from "lucide-react";
+import { ChevronLeft, CirclePlus } from "lucide-react";
+import { useSearchParams, useRouter } from "next/navigation";
 
 import MemoryCard from "@/features/memoryTimeline/components/MemoryCard";
 import { SearchForm } from "@/features/memoryTimeline/components/SearchForm";
@@ -9,13 +10,32 @@ import LoadingWithText from "@/components/LoadingWithText";
 
 export default function Page() {
   const { data, isLoading } = useGetMemories();
-
+  const searchParams = useSearchParams();
+  const router = useRouter();
   const hasMemory = isLoading ? false : data?.data.length !== 0;
   const memories = data?.data ?? [];
 
+  const friendIdFilter = searchParams.get("friend_id");
+  console.log(friendIdFilter);
+  const friendName = searchParams.get("friend_name") ?? "";
+
+  const handleback = () => {
+    router.push("/recent");
+  };
   return (
     <>
-      <SearchForm />
+      {friendIdFilter ? (
+        <div className="relative flex items-center justify-center py-2">
+          <div className="absolute left-1" onClick={handleback}>
+            <ChevronLeft size={28} className="cursor-pointer text-primary" />
+          </div>
+          <h1 className="text-2xl text-center text-primary">
+            {friendIdFilter}の話し相手
+          </h1>
+        </div>
+      ) : (
+        <SearchForm />
+      )}
       <div className="w-full px-6 pt-12">
         <div className="flex flex-col items-center gap-6">
           {isLoading ? (
