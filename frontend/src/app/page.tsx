7428385290c931b318985d4a6c5f0e6a36e5a0cf
chell-uoxou <chelluoxou@gmail.com>
2025-08-24 12:36:52 +0900

    feat 自動でログインページに

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/login");
+  }, [router]);
+
   return (
     <>
-      <h1>shion</h1>
-      <Link href="/login">Login</Link>
+      {/* <h1>shion</h1>
+      <Link href="/login">Login</Link> */}
     </>
   );
 }
